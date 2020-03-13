14fbf2b1d2e46b15205375d54cb407d1073d77bb
Yup <43056998+usnmas@users.noreply.github.com>
2020-03-13 17:27:42 +1300

    Delete run-p4-couch.sh

--- run-p4-couch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 4 peers / 4 CouchDB / 1 organization
-
-# Caliper Run (with channel creation)
-npx caliper benchmark run \
---caliper-workspace . \
---caliper-benchconfig poc/benchmark-config.yaml \
---caliper-networkconfig poc/network-config-p4.yaml
