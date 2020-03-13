1901066314499ce1d7fdbfebf83be60fc15dbab1
Yup <43056998+usnmas@users.noreply.github.com>
2020-03-13 17:28:52 +1300

    Create run-p4.sh

---
+++ run/run-p4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 4 peers / 4 GoLevelDB / 1 organization
+
+# Caliper Run (with channel creation)
+npx caliper benchmark run \
+--caliper-workspace . \
+--caliper-benchconfig poc/benchmark-config.yaml \
+--caliper-networkconfig poc/network-config-p4.yaml
+
+# wait for 5 seconds 
+sleep 5
+
+# change the file name with date and time 
+mv report.html report_`date +%Y%m%d%H%M`_level.html
